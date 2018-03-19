fc5d23212fdaec8c242db9b25770f290dd287212
Aaron Marcher <me@drkhsh.at>
2018-03-20 00:48:10 +0100

    uptime: Port to OpenBSD.
    In OpenBSD uptime gets fetched using sysctl now.

--- components/uptime.c
+++ components/uptime.c
@@ -1,20 +1,45 @@
 /* See LICENSE file for copyright and license details. */
+#include <stdio.h>
 #ifdef __linux__
 #include <sys/sysinfo.h>
+#elif __OpenBSD__
+#include <sys/sysctl.h>
+#include <sys/time.h>
+#endif
 
 #include "../util.h"
 
 const char *
 uptime(void)
 {
+	int h;
+	int m;
+	int uptime = 0;
+#ifdef __linux__
 	struct sysinfo info;
-	int h = 0;
-	int m = 0;
 
 	sysinfo(&info);
-	h = info.uptime / 3600;
-	m = (info.uptime - h * 3600 ) / 60;
+	uptime = info.uptime;
+#elif __OpenBSD__
+	int mib[2];
+	size_t size;
+	time_t now;
+	struct timeval boottime;
+
+	time(&now);
+
+	mib[0] = CTL_KERN;
+	mib[1] = KERN_BOOTTIME;
+
+	size = sizeof(boottime);
+
+	if (sysctl(mib, 2, &boottime, &size, NULL, 0) != -1)
+		uptime = now - boottime.tv_sec;
+	else
+		return NULL;
+#endif
+	h = uptime / 3600;
+	m = (uptime - h * 3600) / 60;
 
 	return bprintf("%dh %dm", h, m);
 }
-#endif
