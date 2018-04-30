3fe1db88922f9c06ee92ad6b6655ca0788aef93b
Aaron Marcher <me@drkhsh.at>
2018-04-30 13:20:24 +0200

    cpu_freq: Port to OpenBSD
    In OpenBSD CPU frequency gets fetched using sysctl now.

--- components/cpu.c
+++ components/cpu.c
@@ -1,10 +1,14 @@
 /* See LICENSE file for copyright and license details. */
-#if defined(__linux__)
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
+#if defined(__OpenBSD__)
+#include <sys/sysctl.h>
+#endif
 
 #include "../util.h"
 
+#if defined(__linux__)
 const char *
 cpu_freq(void)
 {
@@ -61,5 +65,24 @@ cpu_iowait(void)
 	       ((b[0]+b[1]+b[2]+b[3]+b[4]+b[5]+b[6]) - (a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]));
 
 	return bprintf("%d", perc);
+}
+#elif defined(__OpenBSD__)
+const char *
+cpu_freq(void)
+{
+	int freq, mib[2];
+	size_t size;
+
+	mib[0] = CTL_HW;
+	mib[1] = HW_CPUSPEED;
+
+	size = sizeof(freq);
+
+	if (sysctl(mib, 2, &freq, &size, NULL, 0) == -1) {
+		fprintf(stderr, "sysctl 'HW_CPUSPEED': %s\n", strerror(errno));
+		return NULL;
+	}
+
+	return bprintf("%d", freq);
 }
 #endif
