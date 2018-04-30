2ea3d46f1341716daa1370f0f19d1fc571c3e63a
Quentin Rameau <quinq@fifth.space>
2018-04-30 16:13:12 +0200

    Makefile: move OS-specific libs to configure script

--- config.mk
+++ config.mk
@@ -10,9 +10,6 @@ MANPREFIX = ${PREFIX}/share/man
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
-# OpenBSD (uncomment)
-#OSSLIBS = -lossaudio
-
 # flags
 CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Os
 LDFLAGS  = -s
