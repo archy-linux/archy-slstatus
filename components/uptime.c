16e15217c6cb56bd4125c54e645b932a50655b9d
Aaron Marcher <me@drkhsh.at>
2018-07-06 23:47:52 +0200

    uptime: Improve typing and sort headers

--- components/uptime.c
+++ components/uptime.c
@@ -1,19 +1,23 @@
 /* See LICENSE file for copyright and license details. */
-#include <time.h>
+#include <inttypes.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "../util.h"
 
 const char *
 uptime(void)
 {
-	int h, m;
+	uint64_t h, m;
 	struct timespec uptime;
+
 	if (clock_gettime(CLOCK_BOOTTIME, &uptime) < 0) {
 		warn("clock_gettime 'CLOCK_BOOTTIME'");
 		return NULL;
 	}
+
 	h = uptime.tv_sec / 3600;
 	m = uptime.tv_sec % 3600 / 60;
-	return bprintf("%dh %dm", h, m);
+
+	return bprintf("%" PRIu64 "h %" PRIu64 "m", h, m);
 }
