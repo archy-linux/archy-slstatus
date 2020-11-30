dd5bfc3e518a705aba95569537e785bb0d6d7005
Aaron Marcher <me@drkhsh.at>
2020-11-30 20:56:21 +0100

    Add comment for FreeBSD to config.mk
    Apparently `-lkvm` is needed for swap on FreeBSD
    
    Thanks to Micheal Buch and Jason Smith
    (See thread https://lists.suckless.org/dev/1907/33594.html)

--- config.mk
+++ config.mk
@@ -15,6 +15,7 @@ CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE
 CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Os
 LDFLAGS  = -L$(X11LIB) -s
 # OpenBSD: add -lsndio
+# FreeBSD: add -lkvm
 LDLIBS   = -lX11
 
 # compiler and linker
