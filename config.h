67edad45399304180ebf08a196f8edb35a0f8658
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-04-23 07:58:08 +0200

    fix

--- config.h
+++ config.h
@@ -66,7 +66,7 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 	{ run_command, ": %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
-	{ battery_perc, "[BAT %s%%] ", BAT0       },
+	{ battery_perc, "[BAT %s%%] ", "BAT0"     },
 	{ cpu_perc, "[CPU  %s%%]   ", NULL	      },
 	{ ram_perc, "[RAM  %s%%]   ", NULL	      },
 	{ datetime, "%s",           "%a %b %d %r" },
